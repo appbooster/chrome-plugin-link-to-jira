68ca39b5735398ed443e9c6a8f8b8ddc37ad400f
Alexandr Subbotin <kelionweb@gmail.com>
2018-10-02 11:35:32 +0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Github to Jira chrome plugin
+Chrome plugin that replaces all jira issue keys in github PR title to links to that issues:
+
+Before:
+![](before.png)
+
+After 
+![](after.png)
---
+++ content.js
@@ -0,0 +1,6 @@
+var prTitleEl = document.querySelector('.js-issue-title')
+var prTitle = prTitleEl.innerText
+var match = prTitle.match(/[A-Z]+-\d+/)
+if (match) {
+  prTitleEl.innerHTML = prTitle.replace(match[0], `<a href="https://appbooster.atlassian.net/browse/${match[0]}" target="_blank">${match[0]}</a>`)  
+}
