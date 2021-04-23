1419ac03eb9af4e56a45c456c0aa8f88ea191c90
Alexandr Subbotin <kelionweb@gmail.com>
2021-04-23 12:11:29 +0400

    Add support for clickup links

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
 var prTitleEl = document.querySelector('.js-issue-title')
 var prTitle = prTitleEl.innerText
+// Add jira-links
 var match = prTitle.match(/[A-Z0-9]+-\d+/)
 if (match) {
   prTitleEl.innerHTML = prTitle.replace(match[0], `<a href="https://appbooster.atlassian.net/browse/${match[0]}" target="_blank">${match[0]}</a>`)
+}
+
+// Add clickup-links
+match = prTitle.match(/CU-[a-z0-9]+/)
+if (match) {
+  prTitleEl.innerHTML = prTitle.replace(match[0], `<a href="https://app.clickup.com/t/${match[0]}" target="_blank">${match[0]}</a>`)
 }
